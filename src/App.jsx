c1466746e6b7cfe689ef329ed77a8ea5a778d9a8
Liza Dzhus <103603071+lizadzz@users.noreply.github.com>
2024-10-04 10:27:05 +0300

    Update App.jsx
    moving the metamask integration from the App.jsx to Header.jsx + adding comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,21 @@
-import "./App.css";
-import RoutesComponent from "./routes";
-import { Helmet } from "react-helmet";
-import favicon from "./assets/img/tokens/wtheta.png";
-import { useEffect, useState } from "react";
-import Web3 from "web3";
+import "./App.css"; // Importing the main CSS file for styling the application
+import RoutesComponent from "./routes"; // Importing the RoutesComponent for handling different routes
+import { Helmet } from "react-helmet"; // Importing Helmet for managing document head elements
+import favicon from "./assets/img/tokens/wtheta.png"; // Importing a favicon image
 
 function App() {
-    const [web3, setWeb3] = useState(null);
-    const [accounts, setAccounts] = useState([]);
-
-    useEffect(() => {
-        const initWeb3 = async () => {
-            if (window.ethereum) {
-                try {
-                    await window.ethereum.request({ method: 'eth_requestAccounts' });
-                    const web3Instance = new Web3(window.ethereum);
-                    setWeb3(web3Instance);
-                    const accounts = await web3Instance.eth.getAccounts();
-                    setAccounts(accounts);
-                } catch (error) {
-                    console.error("Error connecting to MetaMask:", error);
-                }
-            } else {
-                alert("Please install MetaMask");
-            }
-        };
-        initWeb3();
-    }, []);
-
     return (
         <div className="App">
             <Helmet>
-                <title>Theta Screener</title>
-                <link rel="icon" type="image/png" href={favicon} sizes="16x16" />
+                <title>Theta Screener</title> {/* Set the title of the webpage */}
+                <link rel="icon" type="image/png" href={favicon} sizes="16x16" /> {/* Set favicon */}
             </Helmet>
+            
+            {/* Render routes defined in RoutesComponent */}
             <RoutesComponent />
-            <h1>Connected Accounts</h1>
-            <ul>
-                {accounts.map(account => (
-                    <li key={account}>{account}</li>
-                ))}
-            </ul>
         </div>
     );
 }
 
+// Exporting the App component as default for use in other parts of the application
 export default App;
